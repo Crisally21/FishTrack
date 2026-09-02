025af1f25f1f380a1580c702a7a5ea14bde3658c
Владимир <vovaelandaev@gmail.com>
2026-09-02 15:26:53 +0300

    правка 0 этапа перенос плагина мавен + правка Main.java

--- src/main/java/ru/fishtrack/app/Main.java
+++ src/main/java/ru/fishtrack/app/Main.java
@@ -2,6 +2,7 @@ package ru.fishtrack.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("FishTrack");
+        System.out.println("Версия 1.0.0");
     }
 }
